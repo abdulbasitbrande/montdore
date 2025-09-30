e7f57653404c130cf68109f8f180a4f7bff61055
abdulbasit31 <abdulbasitbaig270@gmail.com>
2025-09-30 15:46:20 +0500

    Header Footer

--- js/theme-fun.js
+++ js/theme-fun.js
@@ -1,28 +1,51 @@
-$(function(){
-    
-    $('.hero-slider').slick({
-        dots: false,
-        arrows:true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 500,
-        pauseOnHover: false,
-        vertical: false,
-        verticalSwiping: false,
-        verticalReverse: false,
-        responsive: [
-            {
-                breakpoint: 767,
-                settings: {
-                    arrows: false,
-                    draggable: false,
-                    swipe: false,
-                }
-            }
-        ]
-    });
+$(function () {
+  // HEADER RESIZE CHECK
+  $(window)
+    .on("resize", function () {
+      if ($(window).width() <= 767 && $("header").hasClass("var-2")) {
+        $("header").removeClass("var-2").addClass("var-1");
+      }
+    })
+    .trigger("resize");
+  // HEADER RESIZE CHECK
 
+  //   BACK TO TOP BTN
+
+  $(".footer .bot-logo").on("click", function (e) {
+    e.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: $("header").offset().top,
+      },
+      600 // duration in ms
+    );
+  });
+
+  //   BACK TO TOP BTN
+
+  $(".hero-slider").slick({
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    pauseOnHover: false,
+    vertical: false,
+    verticalSwiping: false,
+    verticalReverse: false,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+          draggable: false,
+          swipe: false,
+        },
+      },
+    ],
+  });
 });
